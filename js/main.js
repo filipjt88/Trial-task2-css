ae03a31469dbb6201477590652a119b4c6f408a1
filipjt88 <filip88ks@gmail.com>
2023-08-25 13:56:21 +0200

    save

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const body     = document.querySelector("body");
+const slides   = document.querySelectorAll(".slide");
+const leftBtn  = document.querySelector("#left");
+
+const rightBtn = document.querySelector("#right");
+let activeSlide = 0;
+
+function bodyBg() {
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+}
+
+bodyBg();
+
+function currentSlide() {
+    slides.forEach(slide => {
+        slide.classList.remove("active");
+    });
+    slides[activeSlide].classList.add("active");
+}
+
+leftBtn.addEventListener("click", () => {
+    activeSlide--;
+    if(activeSlide < 0) {
+        activeSlide = slides.length - 1;
+    }
+    bodyBg();
+    currentSlide();
+});
+
+rightBtn.addEventListener("click", () => {
+    activeSlide++;
+    if(activeSlide > slides.length -1) {
+        activeSlide = 0;
+    }
+    bodyBg();
+    currentSlide();
+});
